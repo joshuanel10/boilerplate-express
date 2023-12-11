da1e1820bc02fd7d0b91b18bfd09feec42163b83
joshuanel10 <12660812+joshuanel10@users.noreply.github.com>
2023-12-11 17:58:13 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,20 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
 
 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
 
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
+app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
 
 
 
